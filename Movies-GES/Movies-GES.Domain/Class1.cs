c651906ce8da892aad0d331decc77fc229dfd17c
Maurice de Beijer <mauricedb@computer.org>
2014-11-23 20:15:30 +0100

    Initial domain project

---
+++ Movies-GES/Movies-GES.Domain/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies_GES.Domain
+{
+    public class Class1
+    {
+    }
+}
