0bd6904b56b2aeb47939c8ad54201dae281f681a
Maurice de Beijer <mauricedb@computer.org>
2014-11-23 20:36:01 +0100

    Added movie domain object and basic test

---
+++ Movies-GES/Movies-GES.Domain/Domain/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Movies_GES.Domain.Domain
+{
+    public class Movie
+    {
+        private readonly Guid _id;
+        private readonly string _title;
+
+        public Movie(Guid id, string title)
+        {
+            _id = id;
+            _title = title;
+        }
+    }
+}
---
+++ Movies-GES/Movies_GES.Domain.Tests/Domain/MovieSpecs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Movies_GES.Domain.Domain;
+using Xunit;
+
+namespace Movies_GES.Domain.Tests.Domain
+{
+    public class MovieSpecs
+    {
+        [Fact]
+        public void Can_create_a_new_movie()
+        {
+            var movie = new Movie(Guid.NewGuid(), "Some movie");
+
+            movie.Should().NotBeNull();
+        }
+
+    }
+}
