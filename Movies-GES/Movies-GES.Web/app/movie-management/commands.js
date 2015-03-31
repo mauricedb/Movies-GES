7f840b6fba4fa79ad5c6d694467f4d5034026b28
Maurice de Beijer <mauricedb@computer.org>
2015-03-31 14:20:11 +0200

    Removed IEFE

--- Movies-GES/Movies-GES.Web/app/movie-management/commands.js
+++ Movies-GES/Movies-GES.Web/app/movie-management/commands.js
@@ -1,83 +1,82 @@
-﻿(function () {
-    'use strict';
+﻿'use strict';
 
-    var mod = angular.module('movie-commands', ['app-utils']);
+var utils = require('./app-utils');
 
+var mod = angular.module('movie-commands', ['app-utils']);
 
-    mod.factory('movieCommands', function ($http, uuid) {
-        function excute(command) {
-            return $http.put(
-                '/api/commands/' + command.commandId,
-                command,
-                {
-                    headers: {
-                        'Content-Type': 'application/vnd.movies_ges.domain.commands.' + command.commandName.toLowerCase() + '+json'
-                    }
-                }).then(function (e) {
-                    return e;
-                }, function (e) {
-                    console.log(e);
-                });
-        }
+mod.factory('movieCommands', function ($http, uuid) {
+	function excute(command) {
+		return $http.put(
+			'/api/commands/' + command.commandId,
+			command,
+			{
+				headers: {
+					'Content-Type': 'application/vnd.movies_ges.domain.commands.' + command.commandName.toLowerCase() + '+json'
+				}
+			}).then(function (e) {
+				return e;
+			}, function (e) {
+				console.log(e);
+			});
+	}
 
-        function describeMovie(movie) {
+	function describeMovie(movie) {
 
-            return {
-                commandName: 'DescribeMovie',
-                commandId: uuid.v4(),
-                movieId: movie.id,
-                synopsis: movie.synopsis,
-                criticsConsensus: movie.criticsConsensus,
-                year: movie.year || 0
-            };
-        }
+		return {
+			commandName: 'DescribeMovie',
+			commandId: uuid.v4(),
+			movieId: movie.id,
+			synopsis: movie.synopsis,
+			criticsConsensus: movie.criticsConsensus,
+			year: movie.year || 0
+		};
+	}
 
-        function titleMovie(movie) {
-            movie.id = movie.id || uuid.v4();
+	function titleMovie(movie) {
+		movie.id = movie.id || uuid.v4();
 
-            return {
-                commandName: 'TitleMovie',
-                commandId: uuid.v4(),
-                movieId: movie.id,
-                title: movie.title
-            };
-        }
+		return {
+			commandName: 'TitleMovie',
+			commandId: uuid.v4(),
+			movieId: movie.id,
+			title: movie.title
+		};
+	}
 
-        function rateMovieByAudience(movieId, rating) {
-            return {
-                commandName: 'RateMovieByAudience',
-                commandId: uuid.v4(),
-                movieId: movieId,
-                rating: rating
-            };
-        }
+	function rateMovieByAudience(movieId, rating) {
+		return {
+			commandName: 'RateMovieByAudience',
+			commandId: uuid.v4(),
+			movieId: movieId,
+			rating: rating
+		};
+	}
 
-        function rateMovieByCrictics(movieId, rating) {
-            return {
-                commandName: 'RateMovieByCrictics',
-                commandId: uuid.v4(),
-                movieId: movieId,
-                rating: rating
-            };
-        }
+	function rateMovieByCrictics(movieId, rating) {
+		return {
+			commandName: 'RateMovieByCrictics',
+			commandId: uuid.v4(),
+			movieId: movieId,
+			rating: rating
+		};
+	}
 
-        function addDirectorToMovie(movieId, director) {
-            return {
-                commandName: 'AddDirectorToMovie',
-                commandId: uuid.v4(),
-                movieId: movieId,
-                director: director
-            }
-        }
+	function addDirectorToMovie(movieId, director) {
+		return {
+			commandName: 'AddDirectorToMovie',
+			commandId: uuid.v4(),
+			movieId: movieId,
+			director: director
+		}
+	}
 
-        return {
-            titleMovie: titleMovie,
-            describeMovie: describeMovie,
-            rateMovieByAudience: rateMovieByAudience,
-            rateMovieByCrictics: rateMovieByCrictics,
-            addDirectorToMovie: addDirectorToMovie,
-            excute: excute
-        };
-    });
+	return {
+		titleMovie: titleMovie,
+		describeMovie: describeMovie,
+		rateMovieByAudience: rateMovieByAudience,
+		rateMovieByCrictics: rateMovieByCrictics,
+		addDirectorToMovie: addDirectorToMovie,
+		excute: excute
+	};
+});
 
-})();
